a4560576fb3de78a707d9b7e8c5796e168d274e5
Rushil Salian <129928621+Rushilsalian@users.noreply.github.com>
2025-10-17 10:34:57 +0530

    fix4

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './hooks/useAuth'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(r => r.unregister());
+  });
+  caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
+}
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
     <App />
